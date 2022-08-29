07a2395dbbd74a9b32229b4b161e09ccbf9f3cab
gabrielhaguiar <gabrielagui7044@hotmail.com>
2022-08-29 11:02:36 -0300

    first commit

---
+++ assets/scroll.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal({ distance: '15px' });
+sr.reveal('.logo', { duration: 3000, origin: 'right' })
+sr.reveal('.nav-list', { duration: 3000, origin: 'left' })
+sr.reveal('.textos1', { duration: 3000, origin: 'left' })
+sr.reveal('.sobre-mim_descricao', { duration: 3000, origin: 'left' })
+sr.reveal('.sobre-mim_foto', { duration: 3000, origin: 'right' })
+sr.reveal('.projetos-imagens', { duration: 3000, origin: 'bottom' })
+sr.reveal('.habilidades-texto', { duration: 3000, origin: 'bottom' })
+sr.reveal('.projetos-texto', { duration: 3000, origin: 'bottom' })
+sr.reveal('.habilidades-imagens', { duration: 3000, origin: 'left' })
+sr.reveal('.arte1', { duration: 3000, origin: 'right' })
